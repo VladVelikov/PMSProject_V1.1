1edc2266dc756d2cec90d7ccdd2c78dbf992fe4e
Vladimir Velikov <134082559+VladVelikov@users.noreply.github.com>
2024-11-06 22:06:52 +0200

    Migration and minor bug fixed in tbl - SpareParts

--- PMS.Data/Migrations/20241106200531_MinorBugFixedForSpareParts.Designer.cs
+++ PMS.Data/Migrations/20241106200531_MinorBugFixedForSpareParts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PMS.Data;
 
@@ -11,9 +12,11 @@ using PMS.Data;
 namespace PMSWeb.Data.Migrations
 {
     [DbContext(typeof(PMSDbContext))]
-    partial class PMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241106200531_MinorBugFixedForSpareParts")]
+    partial class MinorBugFixedForSpareParts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PMS.Data/Migrations/20241106200531_MinorBugFixedForSpareParts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMSWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorBugFixedForSpareParts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Spareparts",
+                type: "bit",
+                nullable: false,
+                comment: "Soft delete for spare part",
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldComment: "Soft delete fpr spare part");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Spareparts",
+                type: "bit",
+                nullable: false,
+                comment: "Soft delete fpr spare part",
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldComment: "Soft delete for spare part");
+        }
+    }
+}
