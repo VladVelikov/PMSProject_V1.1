03924d3ef9e2e29b8410fb4142bc1b06dd150dd7
Vladimir Velikov <134082559+VladVelikov@users.noreply.github.com>
2024-12-05 10:11:48 +0200

    Comment Only

---
+++ PMS.Common/TODO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Common
+{
+    internal class TODO
+    {
+        ///
+        /// READ ME FILE FILL UP
+        /// ROLES TO BE UPDATED FOR ALL CONTROLLERS
+        /// SEARCH
+        /// RESPONSIVE
+        /// ANTI FORGERY TOKEN
+        /// SEEDER - REWORK TO PROPER ONE
+        /// DASHBOARD REWORK TO NORMAL UI VIEW
+        /// SIGNAL R
+        /// MANUALS TO GOOGLE STORE
+        /// DEPLOY
+        /// 
+
+
+    }
+}
